e40423e5f0e1b36af480cd8179e92dee69ee3d8b
Sergei Miroshnichenko <s.miroshnichenko@yadro.com>
2025-09-30 13:40:48 +0300

    buildroot: linux kernel: opensbi: qemu: Bump everything 2025

--- Edu-Barriers/rv-optimizations-demo-1/0_prereq.sh
+++ Edu-Barriers/rv-optimizations-demo-1/0_prereq.sh
@@ -9,13 +9,13 @@
 # Ubuntu
 #sudo apt install libncurses-dev libextutils-makemaker-cpanfile-perl ninja-build libglib2.0-dev libpixman-1-dev libslirp-dev
 
-git clone -b 2024.11.x git://git.busybox.net/buildroot
+git clone -b 2025.08.x git://git.busybox.net/buildroot
 git clone git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
-cd linux; git checkout v6.13; cd ..
+cd linux; git checkout v6.17; cd ..
 git clone https://github.com/riscv-software-src/opensbi.git
-cd opensbi; git checkout v1.6; cd ..
+cd opensbi; git checkout v1.7; cd ..
 git clone https://github.com/qemu/qemu.git
-cd qemu; git checkout v9.1.1; cd ..
+cd qemu; git checkout v10.0.3; cd ..
 git clone https://github.com/hugsy/gef.git
 git clone https://github.com/dwks/pagemap.git demo_races/pagemap
 sed -i 's/CC = gcc/CC ?= gcc/' demo_races/pagemap/Makefile
