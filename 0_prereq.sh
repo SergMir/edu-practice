bd441838c5db4404b92989a4134e4cf424b6cbff
Sergei Miroshnichenko <s.miroshnichenko@yadro.com>
2026-03-07 14:56:56 +0300

    buildroot: linux kernel: opensbi: qemu: Bump everything 2026

--- 0_prereq.sh
+++ 0_prereq.sh
@@ -9,13 +9,13 @@
 # Ubuntu
 #sudo apt install libncurses-dev libextutils-makemaker-cpanfile-perl ninja-build libglib2.0-dev libpixman-1-dev libslirp-dev
 
-git clone -b 2025.08.x git://git.busybox.net/buildroot
+git clone -b 2026.02.x git://git.busybox.net/buildroot
 git clone git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
-cd linux; git checkout v6.17; cd ..
+cd linux; git checkout v6.19; cd ..
 git clone https://github.com/riscv-software-src/opensbi.git
-cd opensbi; git checkout v1.7; cd ..
+cd opensbi; git checkout v1.8; cd ..
 git clone https://github.com/qemu/qemu.git
-cd qemu; git checkout v10.0.3; cd ..
+cd qemu; git checkout v10.2.0; cd ..
 git clone https://github.com/hugsy/gef.git
 git clone https://github.com/dwks/pagemap.git demo_races/pagemap
 sed -i 's/CC = gcc/CC ?= gcc/' demo_races/pagemap/Makefile
