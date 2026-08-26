ec3f5b487afb7bce1dc70c421ce7b08d255df5d4
Sergei Miroshnichenko <s.miroshnichenko@yadro.com>
2026-08-26 20:04:56 +0300

    buildroot: linux kernel: opensbi: qemu: Bump everything 2026.08

--- 0_prereq.sh
+++ 0_prereq.sh
@@ -5,17 +5,18 @@
 [ -z "`grep safe-path ~/.gdbinit`" ] && echo 'Add "set auto-load safe-path /" to your ~/.gdbinit (or limit to this directory)'
 
 # Fedora
-#sudo dnf install ncurses-devel perl-ExtUtils-MakeMaker ninja-build glib2-devel pixman-devel libslirp-devel perl-IPC-Cmd perl-open lz4 gawk libmpc-devel gmp-devel
+#sudo dnf install ncurses-devel perl-ExtUtils-MakeMaker ninja-build glib2-devel pixman-devel libslirp-devel perl-IPC-Cmd perl-open lz4 gawk libmpc-devel gmp-devel perl-Time-Piece
 # Ubuntu
 #sudo apt install libncurses-dev libextutils-makemaker-cpanfile-perl ninja-build libglib2.0-dev libpixman-1-dev libslirp-dev libipc-run-perl lz4 gawk libmpc-dev libgmp-dev
 
-git clone -b 2026.02.x git://git.busybox.net/buildroot
+git clone git://git.busybox.net/buildroot
+cd buildroot; git checkout 2026.05.1; cd ..
 git clone git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
-cd linux; git checkout v6.19; cd ..
+cd linux; git checkout v7.2; cd ..
 git clone https://github.com/riscv-software-src/opensbi.git
-cd opensbi; git checkout v1.8; cd ..
+cd opensbi; git checkout v1.9; cd ..
 git clone https://github.com/qemu/qemu.git
-cd qemu; git checkout v10.2.0; cd ..
+cd qemu; git checkout v11.1.1; cd ..
 git clone https://github.com/hugsy/gef.git
 git clone https://github.com/dwks/pagemap.git demo_races/pagemap
 sed -i 's/CC = gcc/CC ?= gcc/' demo_races/pagemap/Makefile
